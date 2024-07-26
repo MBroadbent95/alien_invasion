5da629461d4f43e966e5f8b7c1556753a14794e9
Michael Broadbent~ <michael.broadbent95@gmail.com>
2024-07-26 11:07:17 +0100

    updated collision code to better track alien bullet score

--- alien_invasion.py
+++ alien_invasion.py
@@ -98,6 +98,7 @@ class AlienInvasion:
             # Reset the game statistics.
             self.stats.reset_stats()
             self._start_game()
+            self.sb.prep_score()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -160,6 +161,12 @@ class AlienInvasion:
         """Respond to bullet-alien collisions."""
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if collisions:
+            for aliens in collisions.values():
+            # self.stats.score += self.settings.alien_points
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
